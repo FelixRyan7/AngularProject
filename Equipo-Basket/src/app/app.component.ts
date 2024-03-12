bde4366179402e6bd72eee79d591dcf83021335d
Arnau Prats Perelló <aprats.olivera@gmail.com>
2024-03-12 22:50:33 +0100

    Videos funcionando

--- Equipo-Basket/src/app/app.component.ts
+++ Equipo-Basket/src/app/app.component.ts
@@ -1,17 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { MediaComponentComponent } from './media-component/media-component.component';
 
 declare var $: any;
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, MediaComponentComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
+
+export class AppComponent implements OnInit{
   title = 'Equipo-Basket';
 
- 
+    constructor(){
+
+    }
+    ngOnInit(){
+      
+    }
 }
--- Equipo-Basket/src/app/media-component/media-component.component.ts
+++ Equipo-Basket/src/app/media-component/media-component.component.ts
@@ -1,12 +1,48 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild, ElementRef } from "@angular/core";
 
 @Component({
   selector: 'app-media-component',
-  standalone: true,
-  imports: [],
   templateUrl: './media-component.component.html',
-  styleUrl: './media-component.component.css'
+  standalone: true,
+  styleUrls: ['./media-component.component.css']
 })
 export class MediaComponentComponent {
+  name = "Angular";
+  @ViewChild("videoPlayer", { static: false }) videoplayer!: ElementRef;
+  isPlay: boolean = false;
+  toggleVideo(){
+    this.videoplayer.nativeElement.play();
+  }
+  playPause() {
+    var myVideo: any = document.getElementById("my_video_1");
+    if (myVideo.paused) myVideo.play();
+    else myVideo.pause();
+  }
 
+  makeBig() {
+    var myVideo: any = document.getElementById("my_video_1");
+    myVideo.width = 560;
+  }
+
+  makeSmall() {
+    var myVideo: any = document.getElementById("my_video_1");
+    myVideo.width = 320;
+  }
+
+  makeNormal() {
+    var myVideo: any = document.getElementById("my_video_1");
+    myVideo.width = 420;
+  }
+
+  skip(value: any) {
+    let video: any = document.getElementById("my_video_1");
+    video.currentTime += value;
+  }
+
+  restart() {
+    let video: any = document.getElementById("my_video_1");
+    video.currentTime = 0;
+  }
 }
+
+export default MediaComponentComponent;
